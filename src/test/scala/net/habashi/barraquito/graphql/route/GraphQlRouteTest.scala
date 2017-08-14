c46175d9d5294baaebbd927bff777151451ea4e3
Siyavash Habashi <siyavash@habashi.net>
2017-08-14 10:24:29 +0200

    replace asyncFunSpec through AsyncWordSpec

--- src/test/scala/net/habashi/barraquito/graphql/route/GraphQlRouteTest.scala
+++ src/test/scala/net/habashi/barraquito/graphql/route/GraphQlRouteTest.scala
@@ -1,31 +1,28 @@
 package net.habashi.barraquito.graphql.route
 
-import org.scalatest.AsyncFunSpec
+import org.scalatest.AsyncWordSpec
 import spray.json.{JsArray, JsObject, JsString}
 
-class GraphQlRouteTest extends AsyncFunSpec {
+class GraphQlRouteTest extends AsyncWordSpec {
 
-  describe("The GraphQL endpoint") {
-    describe("executes") {
-      it("returns failure for JSONs without query tag") {
-        val jsonQuery = JsObject("foo" -> JsString("bar"))
-        GraphQlRoute.execute(jsonQuery) match {
-          case Left(_) => fail
-          case Right(_) => succeed
-        }
-      }
-
-      it("returns list of coffees for simple coffee-query") {
-        val jsonQuery = JsObject("query" -> JsString("{coffees{name}}"))
-        GraphQlRoute.execute(jsonQuery) match {
-          case Left(response) =>
-            val expected = JsObject("data" -> JsObject("coffees" -> JsArray(JsObject("name" -> JsString("Kazaar")))))
-            response.map { actual => assert(expected == actual) }
-          case Right(_) => succeed
-        }
+  "The GraphQL endpoint" should {
+    "execute returns failure for JSONs without query tag" in {
+      val jsonQuery = JsObject("foo" -> JsString("bar"))
+      GraphQlRoute.execute(jsonQuery) match {
+        case Left(_) => fail
+        case Right(_) => succeed
       }
     }
 
+    "return list of coffees for simple coffee-query" in {
+      val jsonQuery = JsObject("query" -> JsString("{coffees{name}}"))
+      GraphQlRoute.execute(jsonQuery) match {
+        case Left(response) =>
+          val expected = JsObject("data" -> JsObject("coffees" -> JsArray(JsObject("name" -> JsString("Kazaar")))))
+          response.map { actual => assert(expected == actual) }
+        case Right(_) => succeed
+      }
+    }
   }
 
 }
